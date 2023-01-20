dc67c81e76990f935ceeab3c79d893fc5999f383
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-20 11:32:11 +0100

    add task1

---
+++ src/main/java/com/example/Main01.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Main01 {
+    // private static final String QUERY1 = "CREATE TABLE students(
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     email VARCHAR(80) UNIQUE,
+    //     firstName VARCHAR(80),
+    //     lastName VARCHAR(80)
+    // );";
+    // private static final String QUERY2 = "CREATE TABLE exercises(
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     title VARCHAR(80),
+    //     points INT,
+    //     created DATETIME,
+    //     description TEXT
+    // );";
+    // private static final String QUERY3 = "CREATE TABLE students_exercises(
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     student_id INT,
+    //     exercise_id INT,
+    //     FOREIGN KEY (student_id) REFERENCES students (id),
+    //     FOREIGN KEY (exercise_id) REFERENCES exercises (id)
+    // );";
+}
