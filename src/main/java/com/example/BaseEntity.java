966f6b017dba0939e63642f966097ef781781aaa
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-20 12:37:35 +0100

    add task3

---
+++ src/main/java/com/example/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class BaseEntity {
+    private long id;
+
+    public BaseEntity(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
